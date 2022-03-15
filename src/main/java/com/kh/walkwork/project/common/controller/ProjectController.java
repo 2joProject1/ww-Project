fe1aebdd18c35266e9b15482a7ff6888fc3046d5
2joProject1 <2teamproject1@gmail.com>
2022-03-15 12:51:50 +0900

    project controller 경로수정

---
+++ src/main/java/com/kh/walkwork/project/common/controller/ProjectController.java
@@ -0,0 +1,32 @@
+package com.kh.walkwork.project.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProjectController {
+
+	@RequestMapping("project.main")
+	public String main() {
+		return "project/common/projectMain";
+	}
+	
+	@RequestMapping("project.no-read")
+	public String projectNoRead() {
+		return "project/common/projectMainNoRead";
+	}
+	
+			
+	@RequestMapping("project")
+	public String projectDetailView() {
+		return "project/common/projectDetailView";
+	}
+	
+			
+	@RequestMapping("project.taskWrite")
+	public String projectTaskWrite() {
+		return "project/task/projectTaskWrite";
+	}
+			
+			
+}
