80a911ff8d32a552606ebfdf69fed68184618beb
Goryeojin <87753533+Goryeojin@users.noreply.github.com>
2022-03-16 17:25:38 +0900

    header.jsp - 채팅/연락처 div 추가, 프로필 레이어팝업추가 chattingRoom.jsp - 프로필에서 채팅하기 클릭 시 새 창 팝업 뜸 pom.xml - WebSocket 라이브러리 추가 servlet-context.xml - websocket http추가, bean 등록 등

---
+++ src/main/java/com/kh/walkwork/chat/WebSocketServer.java
@@ -0,0 +1,40 @@
+package com.kh.walkwork.chat;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class WebSocketServer extends TextWebSocketHandler{
+	
+	private Set<WebSocketSession> users = new CopyOnWriteArraySet<WebSocketSession>();
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		users.add(session);
+		System.out.println("사용자 접속 : 현재 " + users.size() + "명");
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		
+		// 메세지를 모든 사용자에게 전송(사용자 수만큼 반복하여 전송)
+		
+		TextMessage newMessage = new TextMessage(message.getPayload());
+//		session.sendMessage(newMessage); // 메세지를 발신한 사람에게 다시 메세지를 보냄
+		
+		for(WebSocketSession ws : users) {
+			ws.sendMessage(newMessage);
+		}
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		users.remove(session);
+		System.out.println("사용자 종료 : 현재  " + users.size() + "명");
+	}
+
+}
